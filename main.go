ebf0748023c7d8dbc860dd579543369fef1d7bf9
Joe Elliott <jelliott@qsrautomations.com>
2017-07-22 17:55:21 -0400

    Added AWS client refs

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "github.com/aws/aws-sdk-go/aws/session"
+import "fmt"
 
+func main() {
+	sess := session.Must(session.NewSession())
+
+	if sess != nil {
+		fmt.Println("yay")
+	}
 }
