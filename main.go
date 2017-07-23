ec727bc1ca01998e6afe53f6ab81f8dd2ae0beb0
Joe Elliott <jelliott@qsrautomations.com>
2017-07-23 16:06:04 -0400

    Basic load balancer query

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
-import "github.com/aws/aws-sdk-go/aws/session"
-import "fmt"
+import (
+	"fmt"
+
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elb"
+)
 
 func main() {
-	sess := session.Must(session.NewSession())
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(endpoints.UsEast1RegionID),
+	}))
 
 	if sess != nil {
 		fmt.Println("yay")
+	}
+
+	elbClient := elb.New(sess)
+
+	loadBalancers, err := elbClient.DescribeLoadBalancers(nil)
+
+	if err != nil {
+		log.Fatalf("describeLoadBalancers %v", err)
+	}
+
+	for _, elbDesc := range loadBalancers.LoadBalancerDescriptions {
+		fmt.Printf("%v\n", *elbDesc.LoadBalancerName)
 	}
 }
