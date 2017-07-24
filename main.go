2eec46ad059e2a7bda3a0500d3026af5f3156e7d
Joe Elliott <jelliott@qsrautomations.com>
2017-07-23 23:00:07 -0400

    Added filtering by tags

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,13 +11,13 @@ import (
 )
 
 func main() {
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(endpoints.UsEast1RegionID),
-	}))
+	awsRegion := endpoints.UsEast1RegionID
+	tagName := "KubernetesCluster"
+	tagValue := "myCluster"
 
-	if sess != nil {
-		fmt.Println("yay")
-	}
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion),
+	}))
 
 	elbClient := elb.New(sess)
 
@@ -28,7 +27,34 @@ func main() {
 		log.Fatalf("describeLoadBalancers %v", err)
 	}
 
+	elbNames := make([]*string, 0)
+
 	for _, elbDesc := range loadBalancers.LoadBalancerDescriptions {
-		fmt.Printf("%v\n", *elbDesc.LoadBalancerName)
+		elbNames = append(elbNames, elbDesc.LoadBalancerName)
+	}
+
+	loadBalancerTags, err := elbClient.DescribeTags(&elb.DescribeTagsInput{
+		LoadBalancerNames: elbNames,
+	})
+
+	if err != nil {
+		log.Fatalf("describeTags %v", err)
+	}
+
+	fmt.Println("In Cluster:")
+
+	for _, elbTags := range loadBalancerTags.TagDescriptions {
+		inCluster := false
+
+		for _, kvp := range elbTags.Tags {
+			if *kvp.Key == tagName && *kvp.Value == tagValue {
+				inCluster = true
+				break
+			}
+		}
+
+		if inCluster {
+			fmt.Printf("%v\n", *elbTags.LoadBalancerName)
+		}
 	}
 }
