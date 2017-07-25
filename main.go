110201011a5ee883c9f34b5a0f3b6f9ab574910f
Joe Elliott <jelliott@qsrautomations.com>
2017-07-25 17:00:21 -0400

    Added basic metric querying

--- main.go
+++ main.go
@@ -4,21 +4,25 @@ import (
 	"fmt"
 	"log"
 
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/elb"
 )
 
 func main() {
 	awsRegion := endpoints.UsEast1RegionID
 	tagName := "KubernetesCluster"
-	tagValue := "myCluster"
+	tagValue := "k8s-1.qsrpolarisdev.net"
 
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(awsRegion),
 	}))
 
+	// get load balancer
 	elbClient := elb.New(sess)
 
 	loadBalancers, err := elbClient.DescribeLoadBalancers(nil)
@@ -33,6 +37,7 @@ func main() {
 		elbNames = append(elbNames, elbDesc.LoadBalancerName)
 	}
 
+	// get tags
 	loadBalancerTags, err := elbClient.DescribeTags(&elb.DescribeTagsInput{
 		LoadBalancerNames: elbNames,
 	})
@@ -41,6 +46,8 @@ func main() {
 		log.Fatalf("describeTags %v", err)
 	}
 
+	// filter to only names that belong to the cluster
+	elbNames = make([]*string, 0)
 	fmt.Println("In Cluster:")
 
 	for _, elbTags := range loadBalancerTags.TagDescriptions {
@@ -55,6 +62,43 @@ func main() {
 
 		if inCluster {
 			fmt.Printf("%v\n", *elbTags.LoadBalancerName)
+			elbNames = append(elbNames, elbTags.LoadBalancerName)
 		}
+	}
+
+	// query metrics
+	cwClient := cloudwatch.New(sess)
+
+	now := time.Now()
+	then := now.Add(-60 * time.Minute)
+	metricName := "RequestCount"
+	period := int64(60 * 60)
+	statistic := "Sum"
+	namespace := "AWS/ELB"
+	dimension := "LoadBalancerName"
+
+	for _, elbName := range elbNames {
+		log.Printf("Getting stats for %v", *elbName)
+
+		metricStats, err := cwClient.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
+			Dimensions: []*cloudwatch.Dimension{&cloudwatch.Dimension{
+				Name:  &dimension,
+				Value: elbName,
+			}},
+			StartTime:          &then,
+			EndTime:            &now,
+			ExtendedStatistics: nil,
+			MetricName:         &metricName,
+			Namespace:          &namespace,
+			Period:             &period,
+			Statistics:         []*string{&statistic},
+			Unit:               nil,
+		})
+
+		if err != nil {
+			log.Fatalf("getMetricStatistics %v", err)
+		}
+
+		fmt.Printf("metricStats %v", *metricStats)
 	}
 }
