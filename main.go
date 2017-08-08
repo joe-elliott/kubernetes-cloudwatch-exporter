7a2990b92852a64555ed95f6e4b68f2cb6776b70
Joe Elliott <jelliott@qsrautomations.com>
2017-08-07 23:06:44 -0400

    Got something dumb working

--- main.go
+++ main.go
@@ -4,14 +4,32 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 
 	"kubernetes-cloudwatch-exporter/util"
 
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var settingsFile = flag.String("settings-file", "./settings.json", "Path to load as the settings file")
+var (
+	settingsFile = flag.String("settings-file", "./settings.json", "Path to load as the settings file")
+	promMetrics  = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "k8s_cw_metric",
+			Help: "Cloudwatch Metrics.",
+		},
+		[]string{"elb"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(promMetrics)
+}
 
 func main() {
 	settings, err := util.NewSettings(*settingsFile)
@@ -27,26 +45,45 @@ func main() {
 	getELBNamesFunc := util.MakeELBNamesFunc(settings.TagName, settings.TagValue, sess)
 	getMetricsFunc := util.MakeMetricsFunc(sess)
 
-	elbNamesInCluster, err := getELBNamesFunc()
-
-	if err != nil {
-		log.Fatalf("elbFunc %v", err)
-	}
-
-	fmt.Printf("Found %d load balancers\n", len(elbNamesInCluster))
-
-	for _, elbName := range elbNamesInCluster {
-		for _, elbMetric := range settings.Metrics {
-			log.Printf("Requesting Metrics %v", elbMetric)
-			log.Printf("Requesting for ELB %v", *elbName)
-
-			datapoints, err := getMetricsFunc(elbName, &elbMetric, settings)
+	go func() {
+		for {
+			elbNamesInCluster, err := getELBNamesFunc()
 
 			if err != nil {
-				log.Fatalf("metricsFunc %v", err)
+				log.Fatalf("elbFunc %v", err)
 			}
 
-			log.Printf("Datapoints %v", datapoints)
+			fmt.Printf("Found %d load balancers\n", len(elbNamesInCluster))
+
+			for _, elbName := range elbNamesInCluster {
+				for _, elbMetric := range settings.Metrics {
+					log.Printf("Requesting Metrics %v", elbMetric)
+					log.Printf("Requesting for ELB %v", *elbName)
+
+					datapoints, err := getMetricsFunc(elbName, &elbMetric, settings)
+
+					if err != nil {
+						log.Fatalf("metricsFunc %v", err)
+					}
+
+					log.Printf("Datapoints %v", datapoints)
+
+					// todo: find and choose correct property.
+					//       report all values
+					//       read docs and figure out how to actually do this all correctly
+					if len(datapoints) > 0 && datapoints[0].Average != nil {
+						promMetrics.WithLabelValues(*elbName).Observe(*datapoints[0].Average)
+					} else {
+						promMetrics.WithLabelValues(*elbName).Observe(0)
+					}
+				}
+			}
+
+			time.Sleep(60 * time.Second)
 		}
-	}
+	}()
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
